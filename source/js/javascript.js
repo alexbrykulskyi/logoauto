e34cd1c7bd22ce8a62298d1d0de5eee74bda96e6
Alex <alex.brykulskyi@gmail.com>
2022-03-22 16:22:18 +0200

    content

--- source/js/javascript.js
+++ source/js/javascript.js
@@ -4,4 +4,35 @@ $(document).ready(function() {
    $('.button-toggle').toggleClass('button--active')
    $('.modal-header').toggleClass('display--none')
   });
+
+  $('.slider-reviews').slick({
+    arrows: false,
+    dots: true,
+  });
+});
+
+
+$(window).on('load resize', function() {
+  if ($(window).width() < 768) {
+    $('.slider-list').slick({
+
+    });
+  } else {
+    $('.slider-list').slick("unslick");
+  }
+});
+
+
+$(window).on('load resize', function() {
+  if ($(window).width() < 768) {
+    $('.slider-winners').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 1400,
+      arrows: false
+    });
+  } else {
+    $('.slider-winners').slick("unslick");
+  }
 });
