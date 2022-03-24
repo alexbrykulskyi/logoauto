7adc213a0879a6c7dd485f943603d4a59463f421
Alex <alex.brykulskyi@gmail.com>
2022-03-24 07:12:58 +0200

    slider

--- source/js/javascript.js
+++ source/js/javascript.js
@@ -1,40 +1,38 @@
 $(document).ready(function() {
-  $('.button-toggle').on('click', function(evt){
-    $('.button-toggle').toggleClass('button--active')
-    $('.modal-header').toggleClass('display--none')
+
+  $('.slider-competitions').slick({
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  variableWidth: true,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    }
+    ]
+  });
+
+  $('.slider-winners').slick({
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  variableWidth: true,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    }
+    ]
   });
 
   $('.slider-reviews').slick({
-    arrows: false,
     dots: true,
-  });
-
-  $('.slider-competitions').slick({
-    slidesToShow: 4,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2
-        }
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
-    ]
+    arrows: false,
+    centerMode: true
   });
 });
