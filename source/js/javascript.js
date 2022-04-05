63b7a60559d7162732eb5a2a24715344e1fbc5b4
Alex <alex.brykulskyi@gmail.com>
2022-04-05 22:47:22 +0300

    виправив частину помилок

--- source/js/javascript.js
+++ source/js/javascript.js
@@ -1,38 +1,30 @@
 $(document).ready(function() {
 
+  $('.button-toggle').on('click', function(){
+    $('.popup-wrapper').toggleClass('popup-open'),
+    $('.button-toggle').toggleClass('button--active'),
+    $('.page__header').toggleClass('header--bg'),
+    $('.logo-container__logo, .logo__decor').toggleClass('logo--light')
+  });
+
   $('.slider-competitions').slick({
-  slidesToShow: 4,
-  slidesToScroll: 4,
-  variableWidth: true,
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: "slick",
+      },
+      {
+        breakpoint: 4000,
+        settings: "unslick"
       }
-    }
     ]
   });
 
-  $('.slider-winners').slick({
-  slidesToShow: 4,
-  slidesToScroll: 4,
-  variableWidth: true,
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-      }
-    }
-    ]
-  });
-
-  $('.slider-reviews').slick({
-    dots: true,
-    arrows: false,
-    centerMode: true
-  });
 });
+
+
+
+
+
