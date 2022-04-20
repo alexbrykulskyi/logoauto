63dc0f62cfd6c8c0d711d78666bae0e9360ef787
Alex <alex.brykulskyi@gmail.com>
2022-04-20 12:17:30 +0300

    адаптивність для планшета

--- source/js/javascript.js
+++ source/js/javascript.js
@@ -3,7 +3,8 @@ $(document).ready(function() {
   $('.buttonToggle').on('click', function(){
     $('.navPopupWrapper, .buttonWrapper').toggleClass('popup-open'),
     $('.buttonToggle').toggleClass('buttonToggle--active'),
-    $('.page__header, .header__logoContainer').toggleClass('header--bg'),
+    $('.page__header').toggleClass('header--bg'),
+    $('.header__logoContainer').toggleClass('header__logoContainer--tablet'),
     $('.logo, .logo__decor').toggleClass('logo--light'),
     $('.page').toggleClass('page--noScroll')
 
@@ -19,14 +20,14 @@ $(document).ready(function() {
     $(function(){
       resizeSliderContent = false;
       function resize_slider_content(){
-        if ($(window).width() <= 768 && resizeSliderContent == true) {
+        if ($(window).width() < 767 && resizeSliderContent == true) {
           $('.slider-competitions, .slider-winners').slick({
             slidesToShow: 1,
             arrows: true,
           });
           resizeSliderContent = false;
         }
-        else if ($(window).width() > 768 && resizeSliderContent == false){
+        else if ($(window).width() > 767 && resizeSliderContent == false){
           $('.slider-competitions, .slider-winners').slick('unslick');
           resizeSliderContent = true;
         };
